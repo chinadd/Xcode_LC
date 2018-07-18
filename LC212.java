e14d05cccc21ba81d2e03b9f7679893d35fdfa27
Xing Du <xingdu@Xings-MacBook-Air.local>
2018-07-17 23:42:52 -0400

    my version

--- LC212.java
+++ LC212.java
@@ -1,82 +1,72 @@
-public class Solution {
-	class TrieNode {
-		String word;
-		TrieNode[] children;
+class Solution {
+    class TrieNode {
+        Map<Character, TrieNode> children = new HashMap<>();
+        String word;//pointer to word
 
-		public TrieNode() {
-			children = new TrieNode[26];
-		}
-	}
+        public TrieNode() {
+            this.children = new HashMap<Character, TrieNode>();
+        }
+    }
 
-	class Trie {
-		private TrieNode root;
+    class Trie {
+        private TrieNode root;
 
-		public Trie() {
-			root = new TrieNode();
-		}
+        public Trie() {
+            this.root = new TrieNode();
+        }
 
-		// Inserts a word into the trie.
-		public void insert(String word) {
-			TrieNode cur = root;
-			for (int i = 0; i < word.length(); i++) {
-				int index = word.charAt(i) - 'a';
-				TrieNode child = cur.children[index];
-				if (child == null) {
-					child = new TrieNode();
-					cur.children[index] = child;
-				}
+        public void insert(String word) {
+            TrieNode cur = this.root;
+            for (char c : word.toCharArray()) {
+                cur.children.computeIfAbsent(c, k -> new TrieNode());
+                cur = cur.children.get(c);
+            }
+            cur.word = word; //end of word
+        }
 
-				cur = child;
-			}
-			cur.word = word;
-		}
-
-		public TrieNode searchNextNode(TrieNode node, char c) {
+        public TrieNode searchNextNode(TrieNode node, char c) {
 			if (node == null) {
 				node = root;
 			}
-			int index = c - 'a';
-			return node.children[index];
-		}
-	}
-
-	public List<String> findWords(char[][] board, String[] words) {
-		List<String> result = new ArrayList<>();
-
-		// build Trie
-		Trie trie = new Trie();
-		for (String word : words) {
-			trie.insert(word);
+			return node.children.get(c);
 		}
 
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board[0].length; j++) {
-				dfsFindWords(board, result, trie, null, i, j);
-			}
-		}
-		return result;
-	}
+    }
 
-	private void dfsFindWords(char[][] board, List<String> result, Trie trie, TrieNode node, int x, int y) {
-		char c = board[x][y];
-		TrieNode nextNode = trie.searchNextNode(node, c);
-		if (nextNode == null) {
-			return;
-		}
-		if (nextNode.word != null) {
-			result.add(nextNode.word);
-			nextNode.word = null; // de-duplicate
-		}
-		board[x][y] = '#';
-		for (Point neighbor : getWordSearchNeighbors(board, x, y)) {
-			dfsFindWords(board, result, trie, nextNode, neighbor.x, neighbor.y);
-		}
-		board[x][y] = c;
-	}
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> result = new ArrayList<>();
+        //build trie
+        Trie trie = new Trie();
+        for (String word: words) {
+            trie.insert(word);
+        }
 
-	private List<Point> getWordSearchNeighbors(char[][] board, int x, int y) {
-		List<Point> neighbors = new ArrayList<>(4);
-		for (Point candidate : new Point[] { new Point(x - 1, y), new Point(x + 1, y), new Point(x, y - 1),
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfsFindWords(board, result, trie, null, i, j);
+            }
+        }
+        return result;
+    }
+
+    public void dfsFindWords(char[][] board, List<String> result, Trie trie, TrieNode node, int x, int y) {
+        char c = board[x][y];
+        TrieNode nextNode = trie.searchNextNode(node, c);
+        if (nextNode == null) return;
+        if (nextNode.word != null) {
+            result.add(nextNode.word);
+            nextNode.word = null;
+        }
+        board[x][y] = '#'; //back tracking
+        for (Point neighbor : getWordSearchNeighbors(board, x, y)) {
+            dfsFindWords(board, result, trie, nextNode, neighbor.x, neighbor.y);
+        }
+        board[x][y] = 'c';
+    }
+
+    private List<Point> getWordSearchNeighbors(char[][] board, int x, int y) {
+        List<Point> neighbors = new ArrayList<>();
+        for (Point candidate : new Point[] { new Point(x - 1, y), new Point(x + 1, y), new Point(x, y - 1),
 			    new Point(x, y + 1)
 		}) {
 			if (0 <= candidate.x && candidate.x < board.length && 0 <= candidate.y && candidate.y < board[0].length
@@ -85,5 +75,5 @@ public class Solution {
 			}
 		}
 		return neighbors;
-	}
+    }
 }
